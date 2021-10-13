d5b3924bb2992c43ace520fd27415e3a065bc335
Andy Chan <andy.chan9930@gmail.com>
2021-10-12 23:53:30 -0400

    commit tree 8e1efeb008228cf88364beb46145e07cf1976710

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
-}
+  let string = "string";
+    if (ride > 2500) {
+      string = "No can do."
+    } else if (ride > 2000) {
+      string = "I will gladly take your thirty bucks."
+    } else if (ride <= 400) {
+      string = "This one is on me!"
+    } else {
+      string = "invalid entry"
+    };
+  return string
+};
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
-}
+  return city === "NYC" ? "Ok, sounds good." : "No go."
+};
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+      break;
+    case "not as generous":
+      return "Thank you."
+    default :
+      return "Bye.";
+  };
+};
